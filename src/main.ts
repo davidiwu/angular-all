1c85b08a235b8ac388fd29242815d25e1519c999
David Wu <david.wu@pixelmatic.com>
2020-07-24 17:25:47 +0800

    test private packages

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { Greeter } from '@pixelmatic/dce-common';
 
 if (environment.production) {
   enableProdMode();
+  let greeter = Greeter("hello");
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
